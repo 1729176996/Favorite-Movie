fe803e81a3a7653af1687cebbeda6144c1f557c0
1729176996 <1729176996@qq.com>
2019-10-14 16:01:01 +0800

    33

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 var list = [
     {
-        name:'',
-        cover:'',
-        desc:''
+        name:'sdf',
+        cover:'https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/static/protocol/https/home/img/qrcode/zbios_09b6296.png',
+        desc:'fgg'
     }
 ];
